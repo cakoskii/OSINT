3da3fd5f481a271d421bb407f8cf5b7146749ee5
cakoskii <mehmetbilgic06@icloud.com>
2025-04-28 19:02:58 +0300

    ilk sınıf oluşturuldu

--- osint.py
+++ osint.py
@@ -10,3 +10,14 @@ from PyQt5.QtCore import Qt, QTimer
 from PyQt5.QtGui import QFont, QTextCursor, QIcon, QPixmap
 
 
+class DedektifUygulama(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Dedektif")
+        self.setWindowIcon(QIcon('icon.ico'))
+        self.initUI()
+        
+    
+
+
