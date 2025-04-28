bd96b8a498098447db30bc638241b90ce8103af3
cakoskii <mehmetbilgic06@icloud.com>
2025-04-28 18:53:51 +0300

    kütüphane oluşturma işlemi tamamlandı

--- osint.py
+++ osint.py
@@ -1,6 +1,12 @@
 import sys
 import asyncio  #asenkron programlama,aynı anda birden fazla işin ilerlemesini sağlar
 import aiohttp  #kullanıcıya istek gönderirken kullanılır(bulmak için)
-
+import ssl
+import os
+from PyQt5.QtWidgets import (QApplication, QWidget, QVBoxLayout, QHBoxLayout, QPushButton,
+                             QLineEdit, QLabel, QFileDialog, QGroupBox, QTextBrowser, QDesktopWidget, QProgressBar,
+                             QComboBox, QMessageBox)
+from PyQt5.QtCore import Qt, QTimer
+from PyQt5.QtGui import QFont, QTextCursor, QIcon, QPixmap
 
 
